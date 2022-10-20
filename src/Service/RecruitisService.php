91c4a071d4470260536a5d58f80438b34c0e21c1
Matej Malicky <matej.malicky@gmail.com>
2022-10-20 16:18:09 +0200

    feat: cache external API call results by page

--- src/Service/RecruitisService.php
+++ src/Service/RecruitisService.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 
 use App\Exception\NoJobsFoundException;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
@@ -13,18 +15,25 @@ class RecruitisService
 {
     private HttpClientInterface $client;
 
-    public function __construct(HttpClientInterface $client, string $url, string $token)
-    {
+    private TagAwareCacheInterface $recruitisCache;
+
+    public function __construct(
+        HttpClientInterface $client,
+        TagAwareCacheInterface $recruitisCache,
+        string $url,
+        string $token
+    ) {
         $this->client = $client->withOptions([
             'base_uri' => $url,
             'auth_bearer' => $token,
         ]);
+        $this->recruitisCache = $recruitisCache;
     }
 
     public function retrieveJobs(int $page): array
     {
-        $response = $this->client->request('GET', 'api2/jobs', ['query' => ['page' => $page]]);
-        $jobData = json_decode($response->getContent(), true);
+        $response = $this->getCachedJobsData($page);
+        $jobData = json_decode($response, true);
 
         if ($jobData['meta']['code'] === 'api.response.null') {
             throw new NoJobsFoundException('No jobs were found, try again later');
@@ -40,5 +49,18 @@ class RecruitisService
                 )
             ),
         ];
+    }
+
+    private function getCachedJobsData(int $page)
+    {
+        return $this->recruitisCache->get('job-list-' . $page, function (ItemInterface $item) use ($page) {
+            $response = $this->client->request('GET', 'api2/jobs', ['query' => ['page' => $page]])->getContent();
+            $item->expiresAfter(600);
+
+            // @TODO use user ID for authenticated user
+            $item->tag('user-1');
+
+            return $response;
+        });
     }
 }
